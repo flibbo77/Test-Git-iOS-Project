f6b5695f0cef4592135be5167da03c2abe788fb4
Philipp Roehl <philipp.roehl@TrendAntenna-iMac.local>
2015-10-05 11:06:31 +0200

    Added Test Class2

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Philipp Roehl on 05.10.15.
+//  Copyright © 2015 Continental. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Philipp Roehl on 05.10.15.
+//  Copyright © 2015 Continental. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
