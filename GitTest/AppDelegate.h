af7a5d4fc9b6b4a0a9ef7ad1ddbb1e95e78b00f6
Philipp Roehl <philipp.roehl@trendantenna-imac.local>
2015-10-05 10:52:57 +0200

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Philipp Roehl on 05.10.15.
+//  Copyright © 2015 Continental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Philipp Roehl on 05.10.15.
+//  Copyright © 2015 Continental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
